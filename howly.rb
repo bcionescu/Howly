27905bd8d0dff91f8bcbea75f9566a310ccdbd76
bcionescu <bcionescu@users.noreply.github.com>
2025-11-10 16:53:55 +0100

    Howly can now handle hyperlinks.

--- howly.rb
+++ howly.rb
@@ -1,26 +1,41 @@
-file_path = ARGV[0]
+def read_file
 
-# output_path = "/Users/admin/Desktop/output.txt"
+  file_path = ARGV[0]
 
-if file_path.nil?
-  puts "Usage: ruby howly.rb <path>"
-  exit
+  if file_path.nil?
+    puts "Usage: ruby howly.rb <path>"
+    exit
+  end
+
+  return file_path
+end
+
+def parse_contents(file_path)
+  file_contents = File.read(file_path)
+
+  return file_contents
 end
 
 begin
-  file_contents = File.read(file_path)
+  file_path = read_file
 
-  formatted_file = file_contents
+  file_contents = parse_contents(file_path)
 
-  formatted_file = file_contents.gsub(/\*\*(.+?)\*\*/) do
+  file_contents = file_contents.gsub(/\*\*(.+?)\*\*/) do
     "<b>#{$1}</b>"
   end
 
-  formatted_file = formatted_file.gsub(/\*(.+?)\*/) do
+  file_contents = file_contents.gsub(/\*(.+?)\*/) do
     "<i>#{$1}</i>"
   end
 
-  puts formatted_file
+  file_contents = file_contents.gsub(/\[(.+?)\]\(([^)]+)\)/) do
+    text = $1
+    url = $2
+    %Q(<a href="#{url}" class="body-link" target="_blank" rel="noopener noreferrer">#{text}</a>)
+  end
+
+  puts file_contents
 
 rescue Errno::ENOENT
   puts "File not found: #{file_path}"
--- readme.md
+++ readme.md
@@ -6,10 +6,11 @@ A lightweight Markdown to HTML converter written in Ruby.
 
 - [ ] Automatically add paragraphs
 - [ ] Find and replace headings
-- [ ] Find and replace bold text: `**bold**` or `__bold__`
-- [ ] Find and replace italic text: `*italic*` or `_italic_`
+- [x] Find and replace bold text: `**bold**` or `__bold__`
+- [x] Find and replace italic text: `*italic*` or `_italic_`
 - [ ] Find and replace both bold and italic at the same time: `***boldanditalic***` `___boldanditalic___`
-- [ ] Find and replace hyperlinks
+- [x] Find and replace hyperlinks
+- [ ] Embed YouTube links if found by themselves on a line
 - [ ] Replace Markdown ordered lists with HTML. Check for usage of 1), 2), etc. as well
 - [ ] Take unordered lists into account as well. The user might use `-`, `*`, `+` or `-` to indicate an element in such a list. Bear in mind that indented items are also a possibility. Additionally, unordered lists can start with numbers
 - [ ] Allow for images
